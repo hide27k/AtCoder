a666ff3131ee27dac0a5cd9ccfde02859dcf4366
hide27k <hidehusky1996@gmail.com>
2020-12-29 10:09:43 +0900

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -11,21 +11,37 @@ if [ ! -e "./${1}" ]; then
   mkdir "a"
   cd "a"
   oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_a"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
   cd "../"
 
   mkdir "b"
   cd "b"
   oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_b"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
   cd "../"
 
   mkdir "c"
   cd "c"
   oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_c"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
   cd "../"
 
   mkdir "d"
   cd "d"
   oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_d"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
+  cd "../"
+
+  mkdir "e"
+  cd "e"
+  oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_e"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
+  cd "../"
+
+  mkdir "f"
+  cd "f"
+  oj dl "https://atcoder.jp/contests/${1}/tasks/${1}_f"
+  cp /Users/hkomaki/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp ./
   cd "../"
 else 
   echo "Folder ${1} exists." 1>&2
