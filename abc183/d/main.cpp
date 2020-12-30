e050d3b2fe9f503e0c9b601f0c9827c2e649cee6
hide27k <hidehusky1996@gmail.com>
2020-12-30 10:40:24 +0900

    2020/12/30

--- abc183/d/main.cpp
+++ abc183/d/main.cpp
@@ -1,9 +1,27 @@
-#include <bits/stdc++.h>
+#include<map>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-using ll = long long;
-using P = pair<int,int>;
 
-
+int main(){
+	int n,w;
+	cin >> n >> w;
+	vector<pair<int,int>>event;
+	for(int i=0;i<n;i++){
+		int s,t,p;
+		cin >> s >> t >> p;
+		event.push_back(make_pair(s,p));
+		event.push_back(make_pair(t,-p));
+	}
+	sort(event.begin(), event.end());
+	long long crr=0;
+	for(auto [time,value]:event){
+		crr+=value;
+		if(crr>w){
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+}
