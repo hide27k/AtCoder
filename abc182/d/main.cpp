c555b3ed996f09f5198e6a495ad82e2009ea110c
hide27k <hidehusky1996@gmail.com>
2021-01-01 12:39:15 +0900

    2021/01/01

--- abc182/d/main.cpp
+++ abc182/d/main.cpp
@@ -7,5 +7,19 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
-
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  ll ans = 0;
+  ll s = 0, b = 0;
+  ll max_b = 0;
+  rep(i, n) {
+    b += a[i];
+    max_b = max(max_b, b);
+    ans = max(ans, s + max_b);
+    s += b;
+  }
+  cout << ans << endl;
+  return 0;
 }
