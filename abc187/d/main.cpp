3279a15dce2002b17afb1b8dba11acbed4896962
hide27k <hidehusky1996@gmail.com>
2021-01-05 10:33:17 +0900

    2020/01/05

--- abc187/d/main.cpp
+++ abc187/d/main.cpp
@@ -7,5 +7,23 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  ll n;
+  cin >> n;
+  ll X = 0;
+  vector<ll> x(n);
+  for (ll i = 0; i < n; i++) {
+    ll a, b;
+    cin >> a >> b;
+    X -= a;
+    x[i] = a + a + b;
+  }
+  sort(x.begin(), x.end());
+  ll ans = 0;
+  while (X <= 0) {
+    X += x.back();
+    x.pop_back();
+    ans++;
+  }
 
+  cout << ans << endl;
 }
