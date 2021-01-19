69885153a38d2acc67bd9978da4f2a5cd0a15224
hide27k <hidehusky1996@gmail.com>
2021-01-19 11:45:47 +0900

    2020/01/19

--- abc188/c/main.cpp
+++ abc188/c/main.cpp
@@ -14,8 +14,11 @@ int main() {
   std::vector<int> a (1 << n);
   for (auto &i : a) i = ri();
   int half = 1 << (n - 1);
+  // Where is index with max point?
   int max = std::max_element(a.begin(), a.end()) - a.begin();
+  // If max is in first half? Otherwise...
   auto start = max < half ? a.begin() + half : a.begin();
+  // Find index
   printf("%d\n", (int) (std::max_element(start, start + half) - a.begin() + 1));
 	
 	return 0;
