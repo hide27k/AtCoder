37bf0268e809f15699145b1a955e6521e6f75eb9
hide27k <hidehusky1996@gmail.com>
2021-02-23 15:22:47 -0800

    :sparkles: 2021/2/23

--- abc180/d/main.cpp
+++ abc180/d/main.cpp
@@ -7,5 +7,12 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
-
+  ll x, y, a, b;
+  cin >> x >> y >> a >> b;
+  ll ans = 0;
+  while ((double) a * x <= 2e18 && a * x <= x + b && a * x < y) {
+    x *= a;
+    ans++;
+  }
+  cout << ans + (y - 1 - x) / b << endl;
 }
