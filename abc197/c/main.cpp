bb04b6dde2cb663f2b0404faf295e63d35b02ef9
hide27k <hidehusky1996@gmail.com>
2021-03-30 08:45:18 -0700

    :sparkles: 2021/03/30

--- abc197/c/main.cpp
+++ abc197/c/main.cpp
@@ -7,5 +7,21 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int n; cin >> n;
+  int a[n];
+  rep(i, n) cin >> a[i];
 
+  int res = 2000000000;
+  for (int i = 0; i < 1 << (n - 1); i++) {
+    int xored = 0;
+    int ored = 0;
+    for (int j = 0; j <= n; j++) {
+      if (j < n) ored |= a[j];
+      if (j == n || (i >> j & 1)) xored ^= ored, ored = 0; 
+    }
+    res = min(res, xored);
+  }
+
+  cout << res << endl;
+  return 0;
 }
